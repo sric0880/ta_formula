48979c5f1145b31b262bc02c56012c404fb22f6d
sric <lzqiong@gmail.com>
2024-08-24 21:30:35 +0800

    linux install tools depend on arch

---
+++ tools/install_tools.bash
@@ -0,0 +1,25 @@
+TOOLS="wget sudo"
+echo AUDITWHEEL_POLICY=${AUDITWHEEL_POLICY}
+echo BASE_POLICY=${BASE_POLICY}
+# others may not support yum install, eg. i686/musllinux
+if [ "${AUDITWHEEL_POLICY}" == "manylinux2014" ]; then
+    PACKAGE_MANAGER=yum
+elif [ "${AUDITWHEEL_POLICY}" == "manylinux_2_28" ]; then
+	PACKAGE_MANAGER=dnf
+elif [ "${BASE_POLICY}" == "musllinux" ]; then
+	PACKAGE_MANAGER=apk
+else
+	echo "Unsupported policy: '${AUDITWHEEL_POLICY}'"
+	exit 1
+fi
+
+if [ "${PACKAGE_MANAGER}" == "yum" ]; then
+	yum -y install ${TOOLS}
+elif [ "${PACKAGE_MANAGER}" == "apk" ]; then
+	apk add --no-cache ${TOOLS}
+elif [ "${PACKAGE_MANAGER}" == "dnf" ]; then
+	dnf -y install --allowerasing ${TOOLS}
+else
+	echo "Not implemented"
+	exit 1
+fi
