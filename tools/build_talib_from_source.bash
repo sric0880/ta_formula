535c40848737926daa8259d7849e147a8c38c3ab
sric <lzqiong@gmail.com>
2024-08-24 16:13:41 +0800

    git action talib force build

--- tools/build_talib_from_source.bash
+++ tools/build_talib_from_source.bash
@@ -7,10 +7,6 @@ DEPS_DIR=.
 TA_LIB_TGZ="ta-lib-0.4.0-src.tar.gz"
 TA_LIB_URL="http://prdownloads.sourceforge.net/ta-lib/$TA_LIB_TGZ"
 
-if [[ -d $DEPS_DIR/lib ]]; then
-  echo "Already built"
-  exit 0
-fi
 mkdir -p $DEPS_DIR/tmp
 wget -O "$DEPS_DIR/tmp/$TA_LIB_TGZ" $TA_LIB_URL
 pushd $DEPS_DIR/tmp
